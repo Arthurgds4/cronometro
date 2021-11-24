bd43e80a9329923e559944bfd975e6f0433c22ce
Arthur Gomes <arthurgds4@gmail.com>
2021-11-24 00:34:05 -0300

    testando eventos nos botoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 const cronometro = document.querySelector('.cronometro');
+const play = document.querySelector('#play');
+const pause = document.querySelector('#pause');
+const reset = document.querySelector('#reset');
+
+play.addEventListener('click', function(event){
+    alert('play');
+})
+pause.addEventListener('click', function(event){
+    alert('pause');
+})
+reset.addEventListener('click', function(event){
+    alert('reset');
+})
