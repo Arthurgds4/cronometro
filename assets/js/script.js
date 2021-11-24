be1f1df5281404f8e98298c426201a6edd001841
Arthur Gomes <arthurgds4@gmail.com>
2021-11-24 01:59:11 -0300

    Funcionalidades JavaScrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,37 @@
-const cronometro = document.querySelector('.cronometro');
+function getTimeFromSeconds(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
+
+let segundos = 0;
+let timer;
+function playCronometro() {
+    timer = setInterval(function () {
+        segundos++;
+        cronometro.innerHTML = getTimeFromSeconds(segundos)
+    }, 1000);
+}
+
+const cronometro = document.querySelector('#cronometro');
 const play = document.querySelector('#play');
 const pause = document.querySelector('#pause');
 const reset = document.querySelector('#reset');
+const timeSpent = document.querySelector('#timeSpent');
 
-play.addEventListener('click', function(event){
-    alert('play');
+
+play.addEventListener('click', function (event) {
+    clearInterval(timer);
+    playCronometro();
 })
-pause.addEventListener('click', function(event){
-    alert('pause');
+pause.addEventListener('click', function (event) {
+    clearInterval(timer);
 })
-reset.addEventListener('click', function(event){
-    alert('reset');
+reset.addEventListener('click', function (event) {
+    timeSpent.innerHTML = `Time spent: ${getTimeFromSeconds(segundos)}`;
+    clearInterval(timer);
+    cronometro.innerHTML = '00:00:00';
+    segundos = 0; 
 })
